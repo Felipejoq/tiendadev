4374e4d798ba15b89b33734ce0085b9ab543c840
FelipeJQ <felipe.jofre@inacapmail.cl>
2018-01-22 23:46:50 -0300

    Implementación de la páginación y transformación de los resultados.

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -3,7 +3,9 @@
 namespace App\Traits;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Validator;
 
 trait ApiResponser{
 
@@ -31,6 +33,13 @@ trait ApiResponser{
          */
         $transformer = $collection->first()->transformer;
 
+        $collection = $this->filterData($collection, $transformer);
+
+        $collection = $this->sortData($collection, $transformer);
+
+        //Paginar la colección una vez filtrada pero antes de ser ordenada y transformada.
+        $collection = $this->paginate($collection);
+
         $collection = $this->transformData($collection, $transformer);
 
         return $this->successResponse($collection,$code);
@@ -38,7 +47,6 @@ trait ApiResponser{
     }
 
     protected function showOne(Model $instance, $code = 200){
-
 
         $transformer = $instance->transformer;
 
@@ -52,6 +60,57 @@ trait ApiResponser{
 
         return $this->successResponse(['data' => $message], $code);
 
+    }
+
+    protected function filterData(Collection $collection, $transformer){
+
+        foreach (request()->query() as $query => $value ){
+            $attribute = $transformer::originalAttribute($query);
+
+            if(isset($attribute,$value)){
+                $collection = $collection->where($attribute, $value);
+            }
+
+        }
+
+        return $collection;
+    }
+
+    protected function sortData(Collection $collection, $transformer){
+
+        if (request()->has('sort_by')){
+            $attribute = $transformer::originalAttribute(request()->sort_by);
+            $collection = $collection->sortBy->{$attribute};
+        }
+
+        return $collection;
+    }
+
+    protected function paginate(Collection $collection){
+
+        $rules = [
+            'per_pages' => 'integer|min:2|max:50'
+        ];
+
+        Validator::validate(request()->all(), $rules);
+
+        $page = LengthAwarePaginator::resolveCurrentPage();
+
+        $perPage = 15;
+
+        if (request()->has('per_pages')){
+            $perPage = (int) request()->per_pages;
+        }
+
+        $results = $collection->slice(($page - 1) * $perPage, $perPage)->values();
+
+        $paginated = new LengthAwarePaginator($results, $collection->count(), $perPage, $page, [
+            'path' => LengthAwarePaginator::resolveCurrentPath(),
+        ]);
+
+        $paginated->appends(request()->all());
+
+        return $paginated;
     }
 
     protected function transformData($data, $transformer){
