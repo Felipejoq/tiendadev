79010548f2f7893970f685719479f49840041d20
FelipeJQ <felipe.jofre@inacapmail.cl>
2018-01-23 17:04:18 -0300

    Implementación de Caché para las respuestas y Hateoas.

--- app/Transformers/TransactionTransformer.php
+++ app/Transformers/TransactionTransformer.php
@@ -22,6 +22,29 @@ class TransactionTransformer extends TransformerAbstract
             'fechaCreacion' => (string)$transaction->created_at,
             'fechaActualziacion' => (string)$transaction->update_at,
             'fechaEliminacion' => isset($transaction->deleted_at) ? (string) $transaction->deleted_at : null,
+
+            'links' => [
+                [
+                    'rel' => 'self',
+                    'href' => route('transactions.show', $transaction->id),
+                ],
+                [
+                    'rel' => 'transactions.categories',
+                    'href' => route('transactions.categories.index', $transaction->id),
+                ],
+                [
+                    'rel' => 'transactions.seller',
+                    'href' => route('transactions.sellers.index', $transaction->id),
+                ],
+                [
+                    'rel' => 'buyer',
+                    'href' => route('buyers.show', $transaction->buyer_id),
+                ],
+                [
+                    'rel' => 'products',
+                    'href' => route('products.show', $transaction->product_id),
+                ],
+            ],
         ];
     }
 
